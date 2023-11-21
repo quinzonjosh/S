700cc97a1f6194a2775dcd1b027d4e064b654a0f
quinzonjosh <quinzonjosh261@gmail.com>
2023-11-21 23:20:56 +0800

    initial commit

---
+++ src/main/java/com/sxi/employeemanagementsystem/SxiEmployeeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sxi.employeemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxiEmployeeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SxiEmployeeManagementSystemApplication.class, args);
+	}
+
+}
