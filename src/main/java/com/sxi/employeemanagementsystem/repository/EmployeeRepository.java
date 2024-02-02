ad7937e5fba0944abb8cb1128fe769f64bbdc916
Josh Quinzon <quinzonjosh261@gmail.com>
2024-02-03 02:11:40 +0800

    implemented new ID generation

--- src/main/java/com/sxi/employeemanagementsystem/repository/EmployeeRepository.java
+++ src/main/java/com/sxi/employeemanagementsystem/repository/EmployeeRepository.java
@@ -2,6 +2,11 @@ package com.sxi.employeemanagementsystem.repository;
 
 import com.sxi.employeemanagementsystem.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
+
+    @Query("SELECT MAX(employeeID) FROM Employee")
+    String getLatestId();
+
 }
