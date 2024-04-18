23a30aa1b6a2a54c0907d17817dc8b70c6c81809
Josh Quinzon <quinzonjosh261@gmail.com>
2024-04-18 20:10:30 +0800

    local to remote develop

--- src/main/java/com/sxi/employeemanagementsystem/SxiEmployeeManagementSystemApplication.java
+++ src/main/java/com/sxi/employeemanagementsystem/SxiEmployeeManagementSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SxiEmployeeManagementSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SxiEmployeeManagementSystemApplication.class, args);
 	}
